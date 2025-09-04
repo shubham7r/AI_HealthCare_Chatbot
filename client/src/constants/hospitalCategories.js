3dfaff31070a23fa3fa0123a69bcedf3bf16d3ea
shubham shelke <shubhamshelke779@gmail.com>
2025-09-04 22:47:44 +0530

    ok

---
+++ client/src/constants/hospitalCategories.js
@@ -0,0 +1,15 @@
+// src/constants/hospitalCategories.js
+export const hospitalCategories = [
+  "Ayurveda",
+  "Bones",
+  "Mental Health",
+  "Dermatology",
+  "Cardiology",
+  "Neurology",
+  "Pediatrics",
+  "Gynecology",
+  "ENT",
+  "Dental",
+  "Orthopedics",
+  "General Medicine",
+];
